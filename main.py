4300f5339041ddfa403547146e4a06afb7d6d988
Mark Freedman <mark.freedman@gmail.com>
2023-05-24 20:15:10 -0400

    main function for google cloud

--- main.py
+++ main.py
@@ -1,35 +1,51 @@
-from dotenv import load_dotenv
+import logging
 
+from dotenv import load_dotenv
 
 from src.open_ai_requestor import OpenAiRequestor
 from src.twitter_bot import TwitterBot
 from src.tracery_tweet_message_generator import TraceryTweetMessageGenerator
 
 
-def main():
-    print("loading dotenv")
+def _configure_logger() -> None:
+    # Configure the root logger
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
+
+
+def main(request=None):
+    _configure_logger()
+    logger = logging.getLogger(__name__)
+    logger.debug(f"request: {request}")
     load_dotenv()
-    print("posting AI tweet")
+    logger.info("posting AI tweet")
     post_ai_tweet()
-    print("post tracery tweet")
+    logger.info("post tracery tweet")
     post_tracery_tweet
+    return "Success Tweet function"
 
 
 def post_ai_tweet():
+    logger = logging.getLogger(__name__)
     message_generator = OpenAiRequestor()
     twitter_bot = TwitterBot(TwitterBot.AUTH_KEY_SET_OPEN_AI)
 
     tweet_text = message_generator.get_random_tweet()
 
-    print(f"posting {tweet_text}")
+    logger.info(f"posting AI tweet: {tweet_text}")
     twitter_bot.post_tweet(tweet_text)
 
 
 def post_tracery_tweet():
+    logger = logging.getLogger(__name__)
     message_generator = TraceryTweetMessageGenerator()
     twitter_bot = TwitterBot(TwitterBot.AUTH_KEY_SET_TRACERY)
 
     tweet_text = message_generator.get_random_tweet()
+
+    logger.info(f"posting tracery tweet: {tweet_text}")
     twitter_bot.post_tweet(tweet_text)
 
 
